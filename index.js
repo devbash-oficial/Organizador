1cb155cd113409c9399f3e6a65fb01b87cdfd5f4
TutoMineYT <tutomine56755675@gmail.com>
2023-04-23 20:26:47 +0200

    v-3
    Arreglado tildes al poner carpetas.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require("fs")
 const chalk = require("chalk")
-const readline = require("readline-sync")
+const prompt = require('cli-prompt');
 
 let inicialDir = undefined
 let lastDir = undefined
-let terminado = false
-
 console.clear()
 console.log(chalk.red("==================================================================="))
 console.log(chalk.red("|                                                                 |"))
@@ -22,20 +20,24 @@ console.log("")
 inicialQuestion()
 
 function inicialQuestion() {
-    inicialDir = readline.question(chalk.yellow("¿Que carpeta quieres que se organize?: ")).replaceAll("\\", "/")
-    if (!fs.existsSync(inicialDir) || !fs.statSync(inicialDir).isDirectory()) {
-        console.log(chalk.bgRed("Carpeta no valida."))
-        inicialQuestion()
-    }
-    lastQuestion()
+    prompt(chalk.yellow("¿Que carpeta quieres que se organize?: "), async (val) => {
+        inicialDir = val.replaceAll("\\", "/")
+        if (!fs.existsSync(inicialDir) || !fs.statSync(inicialDir).isDirectory()) {
+            console.log(chalk.bgRed("Carpeta no valida."))
+            return inicialQuestion()
+        }
+        lastQuestion()
+    })
 }
 function lastQuestion() {
-    lastDir = (readline.question(chalk.yellow("¿En donde quieres que se guarden los archivos organizados?: "))).replaceAll("\\", "/")
-    if (!fs.existsSync(lastDir) || !fs.statSync(lastDir).isDirectory()) {
-        console.log(chalk.bgRed("Carpeta no valida."))
-        inicialQuestion()
-    }
-    start()
+    prompt(chalk.yellow("¿En donde quieres que se guarden los archivos organizados?: "), async (val) => {
+        lastDir = val.replaceAll("\\", "/")
+        if (!fs.existsSync(lastDir) || !fs.statSync(lastDir).isDirectory()) {
+            console.log(chalk.bgRed("Carpeta no valida."))
+            return lastQuestion()
+        }
+        start()
+    })
 }
 
 async function start() {
@@ -54,65 +56,72 @@ async function start() {
     organizar()
 
     function organizar() {
-        let answer = readline.question(chalk.yellow(`¿Quieres organizar ${files.length} archivos? (Si/No): `)).toLocaleLowerCase()
-        switch (answer) {
-            case "no":
-                console.log(chalk.bgYellow("Regresando..."))
-                inicialQuestion()
-                break;
-
-            case "si":
-                console.log(chalk.bgGreen("Organizando archivos..."))
-                break;
-
-            default:
-                console.log(chalk.bgRed("Regresando..."))
-                organizar()
-                break;
-        }
+        prompt(chalk.yellow(`¿Quieres organizar ${files.length} archivos? (Si/No): `), async (val) => {
+            let answer = val.toLowerCase()
+            switch (answer) {
+                case "no":
+                    console.log(chalk.bgYellow("Regresando..."))
+                    inicialQuestion()
+                    break;
+    
+                case "si":
+                    console.log(chalk.bgGreen("Organizando archivos..."))
+                    run()
+                    break;
+    
+                default:
+                    console.log(chalk.bgRed("Regresando..."))
+                    organizar()
+                    break;
+            }
+        })
     }
 
-    for (let i = 0; i < files.length; i++) {
-        let File = files[i]
-        if (!await fs.existsSync(lastDir + "/" + File.type.toUpperCase())) {
-            await fs.mkdirSync(lastDir + "/" + File.type.toUpperCase())
-        }
-        console.log(chalk.bgYellow("(MOVIENDO) ->") + " " + chalk.bgCyan("Moviendo archivo " + File.all))
-        await move()
-        await unlink()
-        async function unlink() {
-            console.log(chalk.bgYellow("(MOVIDO)   ->") + " " + chalk.bgGreen("Archivo " + File.all + " movido con exito."))
-            await fs.unlinkSync(`${inicialDir}/${File.all}`)
-        }
-        async function move() {
-            try {
-                await fs.cpSync(`${inicialDir}/${File.all}`, `${lastDir}/${File.type.toUpperCase()}/${File.all}`)
-            } catch (e) {
-                return console.log(chalk.bgYellow("(ERROR)     ->") + " " + chalk.bgRed("Hubo un error al mover el archivo " + File.all + "."))
+    async function run() {
+        for (let i = 0; i < files.length; i++) {
+            let File = files[i]
+            if (!await fs.existsSync(lastDir + "/" + File.type.toUpperCase())) {
+                await fs.mkdirSync(lastDir + "/" + File.type.toUpperCase())
+            }
+            console.log(chalk.bgYellow("(MOVIENDO) ->") + " " + chalk.bgCyan("Moviendo archivo " + File.all))
+            await move()
+            await unlink()
+            async function unlink() {
+                console.log(chalk.bgYellow("(MOVIDO)   ->") + " " + chalk.bgGreen("Archivo " + File.all + " movido con exito."))
+                await fs.unlinkSync(`${inicialDir}/${File.all}`)
+            }
+            async function move() {
+                try {
+                    await fs.cpSync(`${inicialDir}/${File.all}`, `${lastDir}/${File.type.toUpperCase()}/${File.all}`)
+                } catch (e) {
+                    return console.log(chalk.bgYellow("(ERROR)     ->") + " " + chalk.bgRed("Hubo un error al mover el archivo " + File.all + "."))
+                }
             }
         }
+        await closeQuestion()
     }
 
-    await closeQuestion()
 }
 
 function closeQuestion() {
     console.log(chalk.bgYellow("(MOVIDOS)  ->") + " " + chalk.bgGreen("Todos los archivos movidos con exito."))
-    let answer = readline.question(chalk.yellow("¿Quieres repetir este proceso? (Si/No): "))
-    answer = answer.toLowerCase()
-    switch (answer) {
-        case "no":
-            process.exit()
-            break;
-
-        case "si":
-            console.log(chalk.bgYellow("Regresando..."))
-            inicialQuestion()
-            break;
-
-        default:
-            closeQuestion()
-    }
+    prompt(chalk.yellow("¿Quieres repetir este proceso? (Si/No): "), async (val) => {
+        let answer = val.toLowerCase()
+        switch (answer) {
+            case "no":
+                process.exit()
+                break;
+    
+            case "si":
+                console.log(chalk.bgYellow("Regresando..."))
+                inicialQuestion()
+                break;
+    
+            default:
+                closeQuestion()
+        }
+    })
+    
 }
 
 // Hecho por los mas pros de todos > AlexK y TutoMine
